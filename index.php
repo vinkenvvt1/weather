6d7e8f58585dd0d8be449359bba8e1de234126e7
vinkenvvt1 <106219464+vinkenvvt1@users.noreply.github.com>
2024-02-03 21:35:36 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ img {width:36px;}
 </head>
 <body>
   <div class="container">
-  <a class="button" href="map.php">选择城市</a>
+  <a class="button" href="map.html">选择城市</a>
   <br>
 <?php date_default_timezone_set("Asia/Shanghai"); ?>
 <?php
