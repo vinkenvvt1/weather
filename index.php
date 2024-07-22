e36a0dd132fc38e29b79c0c7bb6b65b7def61085
vinkenvvt1 <106219464+vinkenvvt1@users.noreply.github.com>
2024-07-22 19:44:14 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ img {width:36px;}
 </head>
 <body>
 <div class="container">
+
 <a class="button" href="map.html">选择城市</a>
 <br>
 <?php date_default_timezone_set("Asia/Shanghai"); ?>
@@ -21,9 +22,11 @@ $weather_data_raw = file_get_contents("http://t.weather.itboy.net/api/weather/ci
 $weather_data = json_decode($weather_data_raw, true);
 
 if (isset($weather_data["data"])) {
+
 echo '<div class="small_deco">'.$weather_data["cityInfo"]["city"].'</div>';
 $count_forecast = count($weather_data["data"]["forecast"]);
 echo "<table>";
+
 for ($i=0;$i<$count_forecast;$i++) {
 echo "<tr><td>";
 echo date('m-d',time()+86400*$i);
@@ -38,9 +41,10 @@ echo "</td><td>";
 if ($type == "晴"){echo '<img src="2600.png">';}
 if ($type == "阴"){echo '<img src="2601.png">';}
 if ($type == "多云"){echo '<img src="1f325.png">';}
-if ($type == "小雨"||$type == "中雨"||$type == "大雨"){echo '<img src="1f327.png">';}
+if ($type == "小雨"||$type == "中雨"||$type == "大雨"||$type == "暴雨"){echo '<img src="1f327.png">';}
 if ($type == "小雪"||$type == "中雪"||$type == "大雪"){echo '<img src="1f328.png">';}
 if ($type == "霾"){echo '<img src="2601.png">';}
+if ($type == "扬沙"){echo '<img src="1f4a8.png">';}
 echo "</td><td>";
 $wind_fx = $weather_data["data"]["forecast"][$i]["fx"];
 if ($wind_fx == "北风"){echo '<img src="bei.png">';}
@@ -61,10 +65,13 @@ if ($wind_fx == "西西北风"){echo '<img src="xibei.png">';}
 if ($wind_fx == "西西南风"){echo '<img src="xinan.png">';}
 echo "</td></tr>";
 }
+
 echo "</table>";
+
 }
 else echo "citycode error";
 ?>
+
 </div>
 </body>
 </html>
